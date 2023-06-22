0d3e17bb2dc2dd037e969fe4a8f8e208ed66a159
Anmol Raj Singh <77536919+anmolalgorist7110@users.noreply.github.com>
2023-06-22 11:35:21 +0530

    adding quiz-logo and start quiz button

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,23 @@ import 'package:quiz_app/start_screen.dart';
 
 void main (){
   runApp(
-    const MaterialApp(
+     MaterialApp(
       home: Scaffold(
-        body:StartScreen(),
+        body:Container(
+          
+          decoration: const BoxDecoration(
+           gradient: LinearGradient(
+              colors: [
+                   Color.fromARGB(255, 78, 13, 192),
+                    Color.fromARGB(255, 123, 90, 182),
+              ] ,
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+           ),
+
+          ),
+          
+          child: const StartScreen()),
          ),
         ),
         );
